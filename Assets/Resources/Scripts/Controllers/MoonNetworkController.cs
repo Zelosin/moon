3a942738ca19c9c0a91aab577f7c3651758998fe
Zelosin <34071380+Zelosin@users.noreply.github.com>
2022-09-19 21:22:37 +0300

    ms-2 basic tags

---
+++ Assets/Resources/Scripts/Controllers/MoonNetworkController.cs
@@ -0,0 +1,10 @@
+﻿
+public class MoonNetworkController : NetworkController
+{
+    private void Start()
+    {
+        openPort(NetworkInfoStore.MOON_UDP_PORT);
+        connectToPort(NetworkInfoStore.SUN_UDP_PORT);
+        sendMessage(TagStore.CONNECTED);
+    }
+}
